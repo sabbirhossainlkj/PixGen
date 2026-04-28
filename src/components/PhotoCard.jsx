89604e8c06a80a3f61d91c47501f2b82e20427f4
sabbir <sabbirhossain1177034@gmail.com>
2026-04-28 23:27:40 +0600

    add PhotoCard component with image, category and stats

--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -1,17 +1,35 @@
+import { Button, Chip } from "@heroui/react";
 import Image from "next/image";
+import { BiSolidHeart } from "react-icons/bi";
 import { FaDownload } from "react-icons/fa";
 
 const PhotoCard = ({ photo }) => {
   // console.log(photo, 'photo')
-  const { title, imageUrl, likes, downloads } = photo;
+  const { title, imageUrl, likes, downloads,category } = photo;
   return (
     <div className="border p-2 rounded-xl space-y-3">
-      <Image src={imageUrl} alt="title" width={300} height={300}></Image>
+      <div className="relative w-full aspect-square">
+        <Image
+          className="rounded-xl object-cover"
+          src={imageUrl}
+          alt="title"
+          fill
+        ></Image>
+        <Chip size="sm" className="absolute right-2 top-2">{category}</Chip>
+      </div>
       <p className="text-xl font-bold">{title}</p>
       <div className="flex text-gray-400 font-medium justify-between items-center">
-        <p> {likes}</p>
-        <p><FaDownload />{downloads}</p>
+        <p className="flex items-center gap-2">
+          {" "}
+          <BiSolidHeart />
+          {likes}
+        </p>
+        <p className="flex items-center gap-2">
+          <FaDownload />
+          {downloads}
+        </p>
       </div>
+      <Button variant="outline" className={'w-full font-bold'}>Card details</Button>
     </div>
   );
 };
